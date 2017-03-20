a0e60ae6203b21337ea74135629c21026dd86afb
root <root@Grayson.gotham.cawilson.co.uk>
2017-03-20 11:37:21 +0000

    script tweak

--- delugeunrar.sh
+++ delugeunrar.sh
@@ -5,10 +5,10 @@ torrentname=$2
 torrentpath=$3
 
 sleep 60
-date >> /gpfs2/Torrents/execute_script.log
+date >> execute_script.log
 
-echo `date` " Torrent Details: " $torrentname $torrentpath $torrentid  >> /gpfs2/Torrents/execute.log
-/usr/local/bin/unrar x -r -o- $torrentpath/$torrentname/*.rar $torrentpath/Extracted/ >> /gpfs2/Torrents/unrar.log 2>&1; 
-echo `date` " /usr/local/bin/unrar x -r -o- $torrentpath/$torrentname/*.rar $torrentpath/Extracted/" >> /gpfs2/Torrents/execute.log
-/usr/local/bin/unrar x -r -o- $torrentpath/$torrentname/*.part01.rar $torrentpath/Extracted/ >> /gpfs2/Torrents/unrar.log 2>&1;
+echo `date` " Torrent Details: " $torrentname $torrentpath $torrentid  >> execute.log
+/usr/local/bin/unrar x -r -o- $torrentpath/$torrentname/*.rar $torrentpath/Extracted/ >> unrar.log 2>&1; 
+echo `date` " /usr/local/bin/unrar x -r -o- $torrentpath/$torrentname/*.rar $torrentpath/Extracted/" >> execute.log
+/usr/local/bin/unrar x -r -o- $torrentpath/$torrentname/*.part01.rar $torrentpath/Extracted/ >> unrar.log 2>&1;
 
